b3811b683c80602f1b74ef7ef70f4b07112b64fd
Yan Romanikhin <yanromanikhin@hotmail.com>
2013-02-25 00:29:00 +0400

    attempt to fix

--- js/main.js
+++ js/main.js
@@ -1,71 +1,70 @@
 function Calendar() {
-  this.events = [];
+  this.events = {};
+  this.counter = 0;
 
-  // can add same event multiple times
   this.add = function(event) {
-    this.events.push(event)
+    this.events[this.counter++] = event;
+    return this.counter;
   }
 
   this.remove = function(eventID) {
-    this.events.splice(eventID, 1);
+    delete this.events[eventID];
+  }
+
+  this.filterEvents = function(predicate) {
+    var result = [];
+    
+    for (var eventID in this.events) {
+      if (this.events.hasOwnProperty(eventID) && predicate(this.events[eventID])) {
+        result.push(this.events[eventID]);
+      }
+    }
+
+    return result;    
   }
 
   this.getFutureEvents = function() {
-    var currentDate = new Date();
-    
-    return this.events.filter(function(event) { return event.eventDate >= currentDate })
+    return this.filterEvents(function(event) { return event.eventDate >= new Date() });
   }
 
   this.getPastEvents = function() {
-    var currentDate = new Date();
-    
-    return this.events.filter(function(event) { return event.eventDate < currentDate })
+    return this.filterEvents(function(event) { return event.eventDate < new Date() });
   }
 
   // returns events which have at least one tag from tagList
   this.filterByTags = function(tagsList) {
-    function naiveIntersection(firstArray, secondArray) {
-      return firstArray.filter(function(x) { return secondArray.indexOf(x) >= 0 })
+    function hasAtLeastOneTag(event) {
+      for (var tag_index = 0; tag_index < tagsList.length; ++tag_index) {
+        if (event.tags.indexOf(tagsList[tag_index]) != -1) {
+          return true;
+        }
+      }
+
+      return false;
     }
 
-    return this.events.filter(function(event) { return naiveIntersection(event.tags, tagsList).length !== 0 });
+    return this.filterEvents(hasAtLeastOneTag);
   }
 }
 
 function Event(event) {
-  this.name         = event.name;  
-  this.dateCreated  = new Date();  
-  this.eventDate    = event.eventDate; 
-  this.description  = (event.description === undefined) ? 
-                      'one mysterious event' : 
-                      event.description;
-  this.participants = (event.participants === undefined) ?
-                      [] :
-                      event.participants;
-  this.tags         = (event.tags === undefined) ?
-                      [] :
-                      event.tags;
-
-  // this looks like ugly copypaste,
-  // but i'm not sure i know how to avoid it
+  // analogous to dict.update in python.
+  // maybe there is something built in
   this.set = function(updates) {
-    this.name         = (updates.name === undefined) ? 
-                        this.name :
-                        updates.name;
-    this.eventDate    = (updates.eventDate === undefined) ? 
-                        this.eventDate :
-                        updates.eventDate;
-    this.description  = (updates.description === undefined) ? 
-                        this.description : 
-                        updates.description;
-    this.participants = (updates.participants === undefined) ?
-                        this.participants :
-                        updates.participants;
-    this.tags         = (updates.tags === undefined) ?
-                        this.tags :
-                        updates.tags;
-
+    for (var property in updates) {
+      if (updates.hasOwnProperty(property)) {
+        this[property] = updates[property];
+      }
+    }
   }
+
+  var defaults = {description: 'no description available', 
+                  participants: [], 
+                  tags: []}
+
+  this.dateCreated  = new Date(); 
+  this.set(defaults); 
+  this.set(event);
 } 
 
 function Person(person) {
@@ -76,10 +75,10 @@ function Person(person) {
 var person1 = new Person({firstName: 'person1', lastName: 'person111'});
 var person2 = new Person({firstName: 'person2', lastName: 'person222'});
 
-var event1 = new Event({name: 'Happy New Year 2012', eventDate: new Date(2012, 1, 1, 0, 0), participants: [person1, person2], tags: ['xxx', 'new year']})
-var event2 = new Event({name: 'Happy New Year 2015', eventDate: new Date(2015, 1, 1, 0, 0), participants: [person2, person2], tags: ['yyy', 'new year']})
+var event1 = new Event({name: 'Happy New Year 2012', eventDate: new Date(2012, 1, 1, 0, 0), participants: [person1, person2], tags: ['xxx', 'new year']});
+var event2 = new Event({name: 'Happy New Year 2015', eventDate: new Date(2015, 1, 1, 0, 0), participants: [person1, person2], tags: ['yyy', 'new year']});
 
-event2.set({participants: [person1]})
+event2.set({participants: [person1]});
 
 c = new Calendar();
 
