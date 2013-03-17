da47ce9df617ab4bdf6f90e9b145bb0e78f4b745
Anton Ivanov <keyplayer@yandex.ru>
2013-03-18 00:25:26 +0400

    Made calendar to show events from collection instead of static content. Added the ability to add events.

--- js/main.js
+++ js/main.js
@@ -2,19 +2,7 @@
 (function ($, _, Backbone) {
     'use strict';
 
-    /**
-     * Member of event
-     * @param source object with Member properties
-     * @constructor
-     */
-    function Member(source) {
-        this.name = source.name || '';
-        this.lastName = source.lastName || '';
-        this.patronymic = source.patronymic || '';
-        this.email = source.email || '';
-    }
-
-    /**
+	/**
      * Calendar event model
      */
     var EventModel = Backbone.Model.extend({
@@ -81,6 +69,26 @@
         // TODO: localStorage
         //localStorage: new Backbone.LocalStorage('calendar-backbone'),
 
+		/**
+		 * Returns array containing events that happen in the given day
+		 * @param month
+		 * @param day
+		 * @param year
+		 * @returns {array}
+		 */
+		getByDate: function(year, month, day) {
+
+			return this.filter(function (eventModel) {
+
+				var dueDate = eventModel.get('dueDate');
+				return dueDate.getFullYear() === year
+					&& dueDate.getMonth() === month
+					&& dueDate.getDate() === day;
+
+			});
+
+		},
+
         /**
          * Returns array containing past events.
          * @return {Array}
@@ -134,63 +142,160 @@
 
     }),
 
-    EventView = Backbone.View.extend({
+	HeaderView = Backbone.View.extend({
 
-        tagName: 'tr',
-        className: 'eventContainer',
-        template: _.template($('#event-template').html()),
+		el: '#header',
 
-        events: {
-            'click .remove': 'destroyModel'
-        },
+		render: function () {
 
-        initialize: function() {
-            this.listenTo(this.model, 'destroy', this.remove);
-        },
+			var html = $('#header-template').html(),
+				add_event_template_html = $('#add-event-template').html(),
+				cur_date_text = moment().format('DD-MM-YYYY');
+
+			this.$el.html(html);
+
+			this.$add_event_button = this.$el.find('#add-event');
+			this.$add_event_button.popover({
+				placement: 'bottom',
+				html: true,
+				content: _.template(add_event_template_html, {cur_date: cur_date_text})
+			});
+
+		},
+
+		events: {
+			'click #add-event__create': 'create_event'
+		},
+
+		create_event: function () {
+
+			// TODO: validation
+
+			var name = this.$el.find('#add-event__name').val(),
+				date_text = this.$el.find('#add-event__date').val(),
+				date = moment(date_text, 'DD-MM-YYYY').toDate(),
+				event_model = new EventModel({name: name, dueDate: date});
+
+			this.collection.add(event_model);
+			this.$add_event_button.popover('hide');
+		}
+
+	}),
+
+    CellEventView = Backbone.View.extend({
+
+        tagName: 'div',
+        className: 'calendar-cell-event',
+        template: _.template($('#cell-event-template').html()),
 
         render: function () {
 
-            var modelJSON = this.model.toJSON();
-            modelJSON.cid = this.model.cid;
             // TODO: show members properly
-            var html = this.template(modelJSON);
+            var html = this.template({name: this.model.get('name'),
+				members: this.model.get('members')});
             this.$el.html(html);
             return this;
 
-        },
-
-        destroyModel: function () {
-            this.model.destroy();
         }
+
+		/*events: {
+		 'click .remove': 'destroyModel'
+		 },
+
+		 initialize: function() {
+		 this.listenTo(this.model, 'destroy', this.remove);
+		 },
+
+		destroyModel: function () {
+            this.model.destroy();
+        }*/
 
     }),
 
+	// TODO: docstring
+	date_of_last_monday = function(date) {
+
+		var day = date.getDate();
+		if (day === 0) {
+			day = 7;
+		}
+		day -= 1;
+		return new Date(date - day*24*60*60*1000);
+
+	},
+
+	names_of_days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+
     CalendarView = Backbone.View.extend({
 
-        el: '#page',
-        compiledTemplate: _.template($('#calendar-template').html(), {}),
+        el: '#calendar-table',
+
+		initialize: function() {
+			this.listenTo(this.collection, 'add', this.render);
+		},
 
         render: function () {
+			/*jslint browser:true */
+			// TODO: maybe some template?
 
-            this.$el.html(this.compiledTemplate);
-            var calendarBody = $('#calendar-body');
+			var cur_date = new Date(),
+				date_counter = date_of_last_monday(cur_date),
+				week_index, day_index, event_models,
+				row, cell, cell_header;
 
-            _.each(this.collection.models, function(eventModel) {
-                var eventView = new EventView({model: eventModel});
-                calendarBody.append(eventView.render().el);
-            });
+			this.$el.empty();
+			for (week_index=0; week_index < 5; week_index+=1) {
+
+				row = document.createElement('tr');
+				row.className = 'calendar-table-row';
+
+				for (day_index=0; day_index < 7; day_index+=1) {
+
+					cell = document.createElement('td');
+					cell.className = 'calendar-cell';
+
+					cell_header = document.createElement('div');
+					cell_header.className = 'calendar-cell__header';
+					cell_header.innerHTML = date_counter.getDate().toString();
+					if (week_index === 0) {
+						cell_header.innerHTML = names_of_days[day_index] + ', ' + cell_header.innerHTML;
+					}
+					cell.appendChild(cell_header);
+
+					event_models = this.collection.getByDate(
+						date_counter.getFullYear(),
+						date_counter.getMonth(),
+						date_counter.getDate());
+					if (event_models.length > 0) {
+
+						cell.classList.add('calendar-cell-with-events');
+
+						event_models.forEach(function(event_model) {
+
+							var event_view = new CellEventView({model: event_model});
+							cell.appendChild(event_view.render().el);
+
+						});
+
+					}
+
+					row.appendChild(cell);
+
+					date_counter.setDate(date_counter.getDate()+1);
+
+				}
+
+				this.$el.append(row);
+
+			}
+
             return this;
 
         }
 
-        /*initialize: function() {
-         // TODO: can we redraw only a model, not whole calendar?
-         this.listenTo(this.model, 'change', this.render);
-         }*/
-
     }),
 
-    EditEventView = Backbone.View.extend({
+    /*EditEventView = Backbone.View.extend({
 
         el: '#page',
 
@@ -231,13 +336,13 @@
 
         }
 
-    }),
+    }),*/
 
     // predefined event 1 for demonstration
     eventModel1 = new EventModel({
         name: 'Первая лекция',
         description: 'Первая лекция по js',
-        dueDate: new Date(2013, 1, 11, 17, 0),
+        dueDate: new Date(2013, 2, 16, 17),
         tags: ['лекция', 'java script', 'hh']
     }),
 
@@ -245,46 +350,13 @@
     eventModel2 = new EventModel({
         name: 'Вторая лекция',
         description: 'Вторая лекция по js',
-        dueDate: new Date(new Date().getFullYear() + 1, 0, 1, 17),
+        dueDate: new Date(2013, 2, 18, 17),
         tags: ['лекция', 'java script', 'hh', 'important'],
-        members: [
-            {
-                name: 'Anton',
-                lastName: 'Ivanov'
-            },
-            {
-                name: 'Vitaly',
-                lastName: 'Glibin'
-            }]
+        members: ['Anton Ivanov', 'Vitaly Glibin']
     }),
 
     // predefined calendar
-    calendarCollection = new CalendarCollection([eventModel1, eventModel2]),
-
-    Router = Backbone.Router.extend({
-
-        routes: {
-            '': 'home',
-            'new': 'editEvent',
-            'edit/:cid': 'editEvent'
-        },
-
-        home: function () {
-            // TODO: how long does this object live?
-            (new CalendarView({collection: calendarCollection})).render();
-        },
-
-        editEvent: function (cid) {
-            var eventModel = calendarCollection.get(cid);
-            // TODO: how long does this object live?
-            (new EditEventView({model: eventModel, collection: calendarCollection})).render();
-        }
-
-    }),
-
-    router = new Router();
-
-    Backbone.history.start();
+    calendarCollection = new CalendarCollection([eventModel1, eventModel2]);
 
     $.fn.serializeObject = function () {
         var o = {},
@@ -301,6 +373,46 @@
         });
         return o;
     };
+
+	new HeaderView({collection: calendarCollection}).render();
+	new CalendarView({collection: calendarCollection}).render();
+
+	/**
+	 * Member of event
+	 * @param source object with Member properties
+	 * @constructor
+	 */
+	/*function Member(source) {
+		this.name = source.name || '';
+		this.lastName = source.lastName || '';
+		this.patronymic = source.patronymic || '';
+		this.email = source.email || '';
+	}*/
+
+	/*Router = Backbone.Router.extend({
+
+	 routes: {
+	 '': 'home'
+	 //'new': 'editEvent',
+	 //'edit/:cid': 'editEvent'
+	 },
+
+	 home: function () {
+	 // TODO: how long does this object live?
+	 (new CalendarView({collection: calendarCollection})).render();
+	 }
+
+	 /*editEvent: function (cid) {
+	 var eventModel = calendarCollection.get(cid);
+	 // TODO: how long does this object live?
+	 (new EditEventView({model: eventModel, collection: calendarCollection})).render();
+	 }
+
+	 }),
+
+	router = new Router();
+
+	Backbone.history.start(); */
 
 }) (jQuery, _, Backbone);
 
